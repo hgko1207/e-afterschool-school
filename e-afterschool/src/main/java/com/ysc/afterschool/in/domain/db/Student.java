3a29d564c411d8943d24d00a2717e9d1eba77e6f
hgko1207 <khkkhk1207@gmail.com>
2020-06-15 11:15:54 +0900

    초기 설정

---
+++ e-afterschool/src/main/java/com/ysc/afterschool/in/domain/db/Student.java
@@ -0,0 +1,16 @@
+package com.ysc.afterschool.in.domain.db;
+
+import com.ysc.afterschool.in.domain.Domain;
+
+import lombok.Data;
+
+/**
+ * 학생 관리 테이블 도메인
+ * 
+ * @author hgko
+ *
+ */
+@Data
+public class Student implements Domain {
+
+}
---
+++ e-afterschool/src/test/java/com/ysc/afterschool/in/EAfterschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysc.afterschool.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAfterschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
